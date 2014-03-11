bc67b2d12ed8498942cae39765bb761cccea4c15
knod <knodding@gmail.com>
2014-03-11 18:26:57 -0400

    Todo's marked off, examples from elsewhere deleted

--- static/tests.js
+++ static/tests.js
@@ -9,12 +9,9 @@
 * TODO:
 * --- General ---
 * - Make resize function
-* - Look into this: http://jsfiddle.net/sandeeprajoria/x5APH/11/
-* from this:
-* http://stackoverflow.com/questions/10149057/how-to-grab-and-drag-an-element-around-a-circle
 * --- Phases ---
-* - Make circles draggable
-* - Make circles draggable around a centerpoint
+* - [DONE] Make circles draggable
+* - [DONE] Make circles draggable around a centerpoint
 * - Make circles show their degree value in relation
 * to the right side horizon
 * --- Probability ---
@@ -23,6 +20,10 @@
 * 
 * 
 * DONE:
+* - [DONE, it worked by setting the origin not in the element
+* itself] Look into this: http://jsfiddle.net/sandeeprajoria/x5APH/11/
+* from this:
+* http://stackoverflow.com/questions/10149057/how-to-grab-and-drag-an-element-around-a-circle
 * 
 */
 
@@ -61,14 +62,13 @@ $(document).ready(function() {
 			// Get center pos relative to screen
 			var qCenterX = $this.offset().left + qRadius;
 			var qCenterY = $this.offset().top + qRadius;
-			var angleToMouse = ( Math.atan2((mouseX-qCenterX), (mouseY-qCenterY))
-				// - 90
-				// * (180/Math.PI) 
-				);
-			// For getting the text value, add whatever number
-			// will put 0 where you want it
+			var angleToMouse = Math.atan2((mouseX-qCenterX), (mouseY-qCenterY));
+			// For getting the text value: * (180/Math.PI) +
+			// whatever number will put 0 where you want it
 
 			// Put the dot at a certain radius at that angle
+			// (remember to put the arc at the center of the
+			// object by subtracting its own radius)
 			newX = qCenterX + radius * Math.sin(angleToMouse) - circRadius;
 			newY = qCenterY + radius * Math.cos(angleToMouse) - circRadius;
 			$toDrag.css({left: newX, top: newY});
@@ -106,79 +106,6 @@ $(document).ready(function() {
 
 		$toDrag.css({ "left": centerX + "px", "top": centerY + "px" })
 	}
-
-
-
-// $('#demo7_box')
-//         .bind('dragstart',function( event ){
-//                 var data = $( this ).data('dragcircle');
-//                 if ( data ) data.$circle.show(); 
-//                 else {
-//                         data = { 
-//                                 radius: 200, $circle: $([]),
-//                                 halfHeight: $( this ).outerHeight()/2,
-//                                 halfWidth: $( this ).outerWidth()/2
-//                                 };
-//                         data.centerX = event.offsetX + data.radius + data.halfWidth,
-//                         data.centerY = event.offsetY + data.halfHeight,
-//                         // create divs to highlight the path...
-//                         $.each( new Array(72), function( i, a ){
-//                                 angle = Math.PI * ( ( i-36 ) / 36 );
-//                                 data.$circle = data.$circle.add( 
-//                                         $('<div class="point" />').css({
-//                                                 top: data.centerY + Math.cos( angle )*data.radius,
-//                                                 left: data.centerX + Math.sin( angle )*data.radius,
-//                                                 })
-//                                         );
-//                                 });
-//                         $( this ).after( data.$circle ).data('dragcircle', data );
-//                         }
-//                 })
-//         .bind('drag',function( event ){
-//                 var data = $( this ).data('dragcircle'),
-//                 angle = Math.atan2( event.pageX - data.centerX, event.pageY - data.centerY );
-//                 $( this ).css({
-//                         top: data.centerY + Math.cos( angle )*data.radius - data.halfHeight,
-//                         left: data.centerX + Math.sin( angle )*data.radius - data.halfWidth
-//                         });
-//                 })
-//         .bind('dragend',function(){
-//                 $( this ).data('dragcircle').$circle.hide();
-//                 });
-
-
-
-// http://stackoverflow.com/questions/10149057/how-to-grab-and-drag-an-element-around-a-circle
-// function rotateAnnotationCropper(path, mouseX, mouseY, planet){
-//                     //alert(path.left);
-                
-//                     var x = mouseX -planet.offset().left -planet.width()/2;
-//                     var y = -1*(mouseY -planet.offset().top -planet.height()/2);
-//                     var theta = Math.atan2(y,x)*(180/Math.PI);        
-
-
-//                     var cssDegs = convertThetaToCssDegs(theta);
-//                     var rotate = 'rotate(' +cssDegs + 'deg)';
-//                     cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate});
-//                     $('body').on('mouseup', function(event){ $('body').unbind('mousemove')});
-
-//             }
-            
-//             function convertThetaToCssDegs(theta){
-//                 var cssDegs = 90 - theta;
-//                 return cssDegs;
-//             }
-            
-//             $(document).ready(function(){               
-                
-//                 $('#marker').on('mousedown', function(){
-//                     $('body').on('mousemove', function(event){
-//                         rotateAnnotationCropper($('#innerCircle').parent(), event.pageX,event.pageY, $('#marker'));    
-//                     });
-                                        
-//                 });                    
-//             }); 
-
 
 });
 
